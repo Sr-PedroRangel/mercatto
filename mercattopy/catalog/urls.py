dcf9c73b2e35ac4138a4bd680384df8dff75b84a
Pedro Rangel <rangel2049@gmail.com>
2026-04-02 20:09:12 -0300

    Correção baseada na solução do professor.

--- mercattopy/catalog/urls.py
+++ mercattopy/catalog/urls.py
@@ -4,10 +4,15 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("categorias/", views.lista_categorias, name="lista_categorias"),
-    path("produtos/", views.lista_produtos, name="lista_produtos"),
-    path("produtos/<int:pk>/", views.detalhe_produto, name="detalhe_produto"),
-    path("products/create/", views.product_create, name="product_create"),
-    path("products/<int:id>/update/", views.product_update, name="product_update"),
-    path("products/<int:id>/delete/", views.product_delete, name="product_delete"),
+    path("categorias/", views.lista_categorias, name="category_list"),
+
+    path("produtos/", views.lista_produtos, name="product_list"),
+
+    path("produtos/<int:pk>/", views.detalhe_produto, name="product_detail"),
+
+    path("produtos/criar/", views.product_create, name="product_create"),
+
+    path("produtos/<int:pk>/editar/", views.product_update, name="product_update"),
+
+    path("produtos/<int:pk>/excluir/", views.product_delete, name="product_delete"),
 ]
--- mercattopy/catalog/views.py
+++ mercattopy/catalog/views.py
@@ -3,12 +3,12 @@ from .models import Categoria, Produto
 from django.contrib import messages
 from .forms import ProdutoForm
 
+
 def lista_categorias(request):
     categorias = Categoria.objects.all()
     return render(request, "catalog/category_list.html", {
         "categories": categorias
     })
-
 
 
 def lista_produtos(request):
@@ -18,13 +18,13 @@ def lista_produtos(request):
     })
 
 
-
 def detalhe_produto(request, pk):
     produto = get_object_or_404(Produto.objects.select_related("category"), pk=pk)
 
     return render(request, "catalog/product_detail.html", {
         "product": produto
     })
+
 
 def product_create(request):
     if request.method == "POST":
@@ -33,15 +33,16 @@ def product_create(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Produto cadastrado com sucesso!")
-            return redirect("product_list")
+            return redirect("catalog:product_list")
 
     else:
         form = ProdutoForm()
 
     return render(request, "catalog/product_form.html", {"form": form})
 
-def product_update(request, id):
-    product = get_object_or_404(Produto, id=id)
+
+def product_update(request, pk):
+    product = get_object_or_404(Produto, pk=pk)
 
     if request.method == "POST":
         form = ProdutoForm(request.POST, instance=product)
@@ -49,7 +50,7 @@ def product_update(request, id):
         if form.is_valid():
             form.save()
             messages.success(request, "Produto atualizado com sucesso!")
-            return redirect("product_list")
+            return redirect("catalog:product_list")
 
     else:
         form = ProdutoForm(instance=product)
@@ -57,18 +58,18 @@ def product_update(request, id):
     return render(request, "catalog/product_form.html", {"form": form})
 
 
-def product_delete(request, id):
-    product = get_object_or_404(Produto, id=id)
+def product_delete(request, pk):
+    product = get_object_or_404(Produto, pk=pk)
 
     if request.method == "POST":
 
         if product.stock > 0:
             messages.error(request, "Não é possível excluir produto com estoque.")
-            return redirect("product_list")
+            return redirect("catalog:product_list")
 
         product.delete()
         messages.success(request, "Produto excluído com sucesso!")
-        return redirect("product_list")
+        return redirect("catalog:product_list")
 
     return render(request, "catalog/product_confirm_delete.html", {
         "product": product
